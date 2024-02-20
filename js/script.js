2ab2a1494b235c7295edc833c4199bdbe250d8eb
mellouwyellow <jordalij@uci.edu>
2024-02-19 21:38:56 -0800

    add animation to shapes, added gif

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@
 $(document). ready(function() {
 
 //hides box//
+
     $("#box").hide();
 
     //add click function//
+
     $("#button").click(function(){
     $("#box").toggle();
     });
     
     //box animation//
+    
     $("#box").click(function(){
     $(".square").addClass("animate-box");
     setTimeout(function() {
@@ -18,14 +21,20 @@ $(document). ready(function() {
        } , 5000);
        
        });
-    
+    //circle animation//
        $("#name").click(function(){
-        $("#box").css("background-color", "royalblue")
-        $("#box").css("border", "1px solid color")
+        $("#box").css("background-color", "lightgreen")
+        $("#box").css("border", "2px solid color")
         $("#box").css("border-radius", "50%")
     });
     
-    
+    $("#gif").click(function(){
+        $("#box").hide();
+        $("#box").css("background-color", "violet")
+        $(".square").removeClass("animate-box");
+        $("#box").css("border", "2px solid salmon")
+        $("#box").css("border-radius", "100px 75px")
+        });
   
         
     
