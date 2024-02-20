52d54f3b2af489ba4837b410d5767948b4eda6fa
mellouwyellow <jordalij@uci.edu>
2024-02-19 19:42:47 -0800

    added border to shape, box now opens and closes

--- js/script.js
+++ js/script.js
@@ -2,15 +2,34 @@
 
 $(document). ready(function() {
 
+//hides box//
+    $("#box").hide();
 
-$("box").hide();
+    //add click function//
+    $("#button").click(function(){
+    $("#box").toggle();
+    });
+    
+    //box animation//
+    $("#box").click(function(){
+    $(".oval").addClass("animate-box");
+    setTimeout(function() {
+        $(".oval").removeClass("animate-box");  
+       } , 5000);
+       
+       });
+    
 
-$("button").click(function){
-$("#box").toggle();
-});
+    
+  
+        
+    
+    
+    })
 
 
 
 
 
-});
+
+
